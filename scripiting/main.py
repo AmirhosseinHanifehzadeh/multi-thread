39032567c7865f64cfba015271829a9d88ee02e8
Amir Hossein Hanifehzadeh <94629045+AmirhosseinHanifehzadeh@users.noreply.github.com>
2022-07-02 12:22:16 +0430

    Delete scripiting directory

--- scripiting/main.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image, ImageFilter
-
-img = Image.open('./bmw.jpg')
-# filtered_img = img.convert('L')
-# img.thumbnail((400, 400))
-# crooked = filtered_img.rotate(90)
-# img.save("bmw.jpg")
-# print(img.size)
-
